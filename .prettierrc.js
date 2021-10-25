07d86b12f80e330c3e6b0b2da5268895962a7884
Robertosnap <robertosnap@pm.me>
2021-10-25 19:40:11 +0200

    Intial refactor

--- .prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    printWidth: 150,
-    tabWidth: 4,
-    bracketSpacing: true,
-    jsxBracketSameLine: true,
-    singleQuote: false,
-    bracketSpacing: true,
-};
--- src/utils/ethers-helpers.ts
+++
@@ -1,5 +0,0 @@
-import { ethers } from "ethers";
-
-export type Transaction =
-  | ethers.providers.TransactionRequest
-  | ethers.PopulatedTransaction;
