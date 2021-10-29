4966dfe86edba710b5983b75f0edf469aefd86c7
Asbjørn Riddervold <30861967+aridder@users.noreply.github.com>
2021-10-29 13:25:31 +0200

    Supply balances from captable with shareholder data fra helpers (#21)
    * merge shareholders from helpers and balances from thegraph
    
    * show user role below balance table and supply balance table with shareholder info
    
    Co-authored-by: Robertosnap <robertosnap@pm.me>

--- src/components/CapTableBalances.tsx
+++ src/components/CapTableBalances.tsx
@@ -1,101 +1,173 @@
-import React, { useContext } from 'react';
-import { Box, Button, DataTable, Paragraph, Spinner, Text } from 'grommet';
-import { CapTableGraphQL, CapTableGraphQLTypes } from '../utils/CapTableGraphQL.utils';
-import { useQuery } from 'graphql-hooks';
-import { FormatEthereumAddress } from './FormatEthereumAddress';
-import { ethers } from 'ethers';
-import { Edit } from 'grommet-icons';
-import useInterval from '../utils/useInterval';
-import { BrokContext } from '../context/BrokContext';
-import { useAsyncEffect } from 'use-async-effect';
-import { ExportExcel } from '../utils/ExportExcel';
+import { ethers } from "ethers";
+import { useQuery } from "graphql-hooks";
+import { Box, Button, DataTable, Paragraph, Spinner, Text } from "grommet";
+import { Edit } from "grommet-icons";
+import React, { useContext, useEffect, useState } from "react";
+import { useAsyncEffect } from "use-async-effect";
+import { BrokContext, CapTableBalance, getRoleName, ROLE, Shareholder } from "../context/BrokContext";
+import { CapTableGraphQL, CapTableGraphQLTypes } from "../utils/CapTableGraphQL.utils";
+import { ExportExcel } from "../utils/ExportExcel";
+import useInterval from "../utils/useInterval";
 var debug = require("debug")("component:CapTableBalances");
 
 interface Props {
-    capTableAddress: string
-    name: string
+    capTableAddress: string;
+    name: string;
 }
 
 export const CapTableBalances: React.FC<Props> = ({ ...props }) => {
-    const { loading, error, data, refetch } =
-        useQuery<CapTableGraphQLTypes.BalancesQuery.Response>(CapTableGraphQL.BALANCES_QUERY(props.capTableAddress));
+    const { loading, error, data, refetch } = useQuery<CapTableGraphQLTypes.BalancesQuery.Response>(
+        CapTableGraphQL.BALANCES_QUERY(props.capTableAddress)
+    );
+    const [shareholdersLoading, getShareholdersLoading] = useState(false);
+    const [role, setRole] = useState<ROLE>("PUBLIC");
+    const [shareholders, setShareholders] = useState<Shareholder[]>([]);
+    const [capTableBalance, setCapTableBalance] = useState<CapTableBalance[]>([]);
 
-    const { getCaptableShareholders } = useContext(BrokContext)
+    const { getCaptableShareholders } = useContext(BrokContext);
+
+    useEffect(() => {
+        if (shareholdersLoading || loading) return;
+        if (!data) return;
+
+        const _capTableBalance = mergeBalancesWithShareholderDate(data.balances, shareholders);
+        setCapTableBalance(_capTableBalance);
+    }, [shareholdersLoading, loading, data, shareholders]);
 
     useInterval(() => {
-        refetch()
-    }, 4000)
+        refetch();
+    }, 4000);
+
+    const mergeBalancesWithShareholderDate = (balances: CapTableGraphQLTypes.BalancesQuery.Balance[], shareholders: Shareholder[]) => {
+        const isSameLength = balances.length === shareholders.length;
+        debug("Balances and shareholder is same length", isSameLength);
+
+        return balances
+            .map((balance) => {
+                const shareholder = shareholders.find((s) => s.address === balance.tokenHolder.address);
+                if (!shareholder) {
+                    console.warn("Could not find shareholder belonging to balance");
+                    return undefined
+                }
+                return {
+                    ...shareholder,
+                    ...balance,
+                };
+            })
+            .filter((obj): obj is CapTableBalance => !!obj);
+    };
 
     useAsyncEffect(async (isMounted) => {
         try {
-            const response = await getCaptableShareholders(props.capTableAddress)
+            getShareholdersLoading(true);
+            const response = await getCaptableShareholders(props.capTableAddress);
+
             if (response.status === 200) {
                 if (isMounted()) {
-                    console.log(response.data)
+                    setShareholders(response.data.shareholders);
+                    setRole(response.data.yourRole as ROLE);
+                    getShareholdersLoading(false);
+                    debug("role", response.data.yourRole);
                 }
             }
         } catch (error: any) {
             if ("message" in error) {
-                debug(error.message)
+                debug(error.message);
             } else {
-                debug("error in getUnclaimedShares", error)
+                debug("error in getUnclaimedShares", error);
             }
         }
+    }, []);
 
-    }, [])
 
+    const roleDependendtColums= () => {
+        return [
+            // {
+            //     property: "address",
+            //     header: <Text>ID</Text>,
+            //     render: (data) => <FormatEthereumAddress address={data.tokenHolder.address}></FormatEthereumAddress>,
+            // },
+            {
+                property: "name",
+                header: <Text>Navn</Text>,
+                render: (data : CapTableBalance) => data.name,
+            },
+            {
+                property: "city",
+                header: <Text>By</Text>,
+                render: (data: CapTableBalance) => data.city,
+            },
+            {
+                property: "postcode",
+                header: <Text>Postkode</Text>,
+                render: (data: CapTableBalance) => data.postcode ?? "",
+            },
+            {
+                property: "email",
+                header: <Text>Epost</Text>,
+                render: (data: CapTableBalance) => data.email ?? "",
+            },
+            {
+                property: "birthday",
+                header: <Text>Født</Text>,
+                render: (data: CapTableBalance) => data.birthdate ?? "",
+            },
+            {
+                property: "balance",
+                header: <Text>Aksjer</Text>,
+                render: (data: CapTableBalance) => ethers.utils.formatEther(data.amount),
+            },
+            {
+                property: "balanceByPartition",
+                header: <Text>Aksjeklasser</Text>,
+                render: (data: CapTableBalance) => data.partition,
+            },
+            {
+                property: "virtual",
+                header: "",
+                render: (data: CapTableBalance) => {
+                    return (
+                        <Button
+                            icon={<Edit></Edit>}
+                            // onClick={() => setEditEntity(data.tokenHolder.address)}
+                            // disabled={
+                            //     data.capTable.owner.toLowerCase() !== address?.toLowerCase()
+                            // }
+                        ></Button>
+                    );
+                },
+            },
+        ].filter(row => {
+            if(role !== "BOARD_DIRECTOR"){
+                if(["identifier", "email", "postcode"].includes(row.property)){
+                    return false
+                }
+            }
+            return true
+        })
+    }
 
+ 
     return (
         <Box>
             {error && <Paragraph>Noe galt skjedde</Paragraph>}
 
-            {data &&
+            {data && 
                 <DataTable
-                    data={data ? data.balances : []}
-                    primaryKey={false}
-                    columns={[
-                        {
-                            property: "address",
-                            header: <Text>ID</Text>,
-                            render: (data) => (
-                                <FormatEthereumAddress address={data.tokenHolder.address}></FormatEthereumAddress>
-                            ),
-                        },
-                        {
-                            property: "balance",
-                            header: <Text>Aksjer</Text>,
-                            render: (data) => ethers.utils.formatEther(data.amount),
-                        },
-                        {
-                            property: "balanceByPartition",
-                            header: <Text>Aksjeklasser</Text>,
-                            render: (data) => data.partition,
-                        },
-                        {
-                            property: "virtual",
-                            header: "",
-                            render: (data) => {
-                                return (
-                                    <Button
-                                        icon={<Edit></Edit>}
-                                    // onClick={() => setEditEntity(data.tokenHolder.address)}
-                                    // disabled={
-                                    //     data.capTable.owner.toLowerCase() !== address?.toLowerCase()
-                                    // }
-                                    ></Button>
-                                );
-                            },
-                        },
-                    ]}
-                >
-                </DataTable>
-            }
-            {data && <ExportExcel capTableName={props.name} data={data} />}
+                data={capTableBalance ? capTableBalance : []}
+                primaryKey={false}
+                columns={roleDependendtColums()}
+                ></DataTable>
+                    }
+            {capTableBalance && (
+                <Box fill="horizontal" direction="row" margin="small" align="center" justify="between">
+                    <Text size="small" color="blue">Vises som {getRoleName(role).toLocaleLowerCase()}</Text>
+                    <ExportExcel capTableName={props.name} data={capTableBalance} />
+                </Box>
+            )}
             <Box margin="small" align="center" height="small">
-                {loading &&
-                    <Spinner></Spinner>
-                }
+                {loading || (shareholdersLoading && <Spinner></Spinner>)}
             </Box>
         </Box>
-    )
-}
+    );
+};
