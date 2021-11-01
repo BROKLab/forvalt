a48e74b245280118b78da0a326f1afc8a435552d
aridder <aridder@pm.me>
2021-11-01 16:14:26 +0100

    typo + formatting

--- src/components/CapTableDetails.tsx
+++ src/components/CapTableDetails.tsx
@@ -1,20 +1,19 @@
-import React from 'react';
-import { Box, Grid, Text } from 'grommet';
-import { FormatEthereumAddress } from './FormatEthereumAddress';
+import React from "react";
+import { Box, Grid, Text } from "grommet";
+import { FormatEthereumAddress } from "./FormatEthereumAddress";
 
 interface Props {
     data: {
-        name: string
-        organizationNuber: string
-        boardDirector: string
-        active: boolean
-        totalSupply: string
-        isCurrentWalletConntroller: boolean
-    }
+        name: string;
+        organizationNuber: string;
+        boardDirector: string;
+        active: boolean;
+        totalSupply: string;
+        isCurrentWalletConntroller: boolean;
+    };
 }
 
 export const CapTableDetails: React.FC<Props> = ({ data, ...props }) => {
-
     return (
         <Box gap="small">
             <Grid columns={["small", "flex"]}>
@@ -22,26 +21,27 @@ export const CapTableDetails: React.FC<Props> = ({ data, ...props }) => {
                 <Text weight="bold">{data.name}</Text>
             </Grid>
             <Grid columns={["small", "flex"]}>
-                <Text >Orginisasjonsnummer</Text>
+                <Text>Organisasjonsnummer</Text>
                 <Text weight="bold">{data.organizationNuber}</Text>
             </Grid>
             <Grid columns={["small", "flex"]}>
-                <Text >Styreleder</Text>
-                <Text weight="bold"><FormatEthereumAddress address={data.boardDirector}></FormatEthereumAddress></Text>
+                <Text>Styreleder</Text>
+                <Text weight="bold">
+                    <FormatEthereumAddress address={data.boardDirector}></FormatEthereumAddress>
+                </Text>
             </Grid>
             <Grid columns={["small", "flex"]}>
-                <Text >Aktivt</Text>
+                <Text>Aktivt</Text>
                 <Text weight="bold">{data.active ? "Ja" : "Nei"}</Text>
             </Grid>
             <Grid columns={["small", "flex"]}>
-                <Text >Antall aksjer</Text>
+                <Text>Antall aksjer</Text>
                 <Text weight="bold">{data.totalSupply}</Text>
             </Grid>
             <Grid columns={["small", "flex"]}>
-                <Text >Skrive rettigheter</Text>
+                <Text>Skriverettigheter</Text>
                 <Text weight="bold">{data.isCurrentWalletConntroller ? "Ja" : "Nei"}</Text>
             </Grid>
-
         </Box>
-    )
-}
+    );
+};
