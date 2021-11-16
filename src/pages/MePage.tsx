182acc67fcbca103cd89225efd7ecd3d0b8dc8fa
Jonas Johan Solsvik <jonas@symfoni.solutions>
2021-11-16 14:12:38 +0100

    Tillat Brønnøysundregistrene Aksjeeierbok å hente dine aksjer

--- src/pages/MePage.tsx
+++ src/pages/MePage.tsx
@@ -17,7 +17,7 @@ export const MePage: React.FC<Props> = () => {
         <Box gap="small">
             <Heading level={1}>Mine aksjer</Heading>
             {hasPermission && <MeBalances address={address} />}
-            {!hasPermission && <Button href="#access-permission-request" size="small" label="Be om tilgang"></Button>}
+            {!hasPermission && <Button href="#access-permission-request" size="small" label="Tillat Brønnøysundregistrene Aksjeeierbok å hente dine aksjer"></Button>}
         </Box>
     );
 };
